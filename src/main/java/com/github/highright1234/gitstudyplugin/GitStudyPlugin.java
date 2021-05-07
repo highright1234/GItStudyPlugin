8aa4666620da4b4c679c7fc79f922a308187b3cb
highright1234 <tickiock08@gmail.com>
2021-05-08 08:35:47 +0900

    init commit

---
+++ src/main/java/com/github/highright1234/gitstudyplugin/GitStudyPlugin.java
@@ -0,0 +1,17 @@
+package com.github.highright1234.gitstudyplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GitStudyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
