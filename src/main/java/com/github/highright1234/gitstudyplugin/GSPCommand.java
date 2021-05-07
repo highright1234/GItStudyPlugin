3e90b9df6f4d0d2b43f300384ef8283976714b2f
highright1234 <tickiock08@gmail.com>
2021-05-08 08:43:08 +0900

    add command

---
+++ src/main/java/com/github/highright1234/gitstudyplugin/GSPCommand.java
@@ -0,0 +1,15 @@
+package com.github.highright1234.gitstudyplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class GSPCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        sender.sendMessage("Hello world!");
+        return false;
+    }
+}
--- src/main/java/com/github/highright1234/gitstudyplugin/GitStudyPlugin.java
+++ src/main/java/com/github/highright1234/gitstudyplugin/GitStudyPlugin.java
@@ -2,10 +2,13 @@ package com.github.highright1234.gitstudyplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class GitStudyPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Objects.requireNonNull(getCommand("gitstudyplugin")).setExecutor(new GSPCommand());
         // Plugin startup logic
 
     }
